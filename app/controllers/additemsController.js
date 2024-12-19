3d6c5782c96fab7f1688f0d6d3cbcbe1a08b4530
Rasagna205 <rasagnabathyala0402@gmail.com>
2024-12-19 20:52:07 +0530

    make changes in additemcontroller

--- app/controllers/additemsController.js
+++ app/controllers/additemsController.js
@@ -38,6 +38,37 @@ const addItem=async(req,res)=>
 const viewofItems=async(req,res)=>
 {
     const viewItems=await prisma.item.findMany()
-    res.json({message:"these items are available in database :" , viewItems})
+    res.status(200).json({message:"these items are available in database :" , viewItems})
 }
-module.exports={addItem,viewofItems};
+const updateItems=async(req,res)=>
+{
+    try{
+        const normalizeBody=Object.fromEntries(
+            Object.entries(req.body).map(([key,value]) => [key.toLowerCase(),value]));
+        
+    const id=req.params.id;
+    const{itemname,quantity,category,saleprice,mrp}=normalizeBody;
+    const itemUpdate=await prisma.item.update({
+        where:{id:parseInt(id)},
+            data:{itemName:itemname,quantity,category,salePrice:saleprice,mrp}
+        
+    })
+
+    return res.status(200).json({message:"itemName is updated : ", itemUpdate})
+}
+    catch(error)
+    {
+        console.log("error occurs :",error.message);
+        return res.status(400).json({message:"failed to update an item"})
+    }
+}
+
+const deleteItems=async(req,res)=>
+{
+    const id=req.params.id;
+    const deletionOfItem=await prisma.item.delete({
+        where:{id:parseInt(id)}
+    })
+    res.status(200).json({message:"successfully deleted the item : ",deletionOfItem})
+}
+module.exports={addItem,viewofItems,updateItems,deleteItems};
--- app/routes/itemRoute.js
+++ app/routes/itemRoute.js
@@ -1,7 +1,10 @@
 const express=require('express')
 const router=express.Router();
-const {addItem,viewofItems}=require('../controllers/additemsController')
+const {addItem,viewofItems,updateItems,deleteItems}=require('../controllers/additemsController')
 router.post('/additem',addItem)
 router.get('/view',viewofItems)
+router.put('/update/:id',updateItems)
+router.delete('/delete/:id',deleteItems)
+
 module.exports=router;
 
